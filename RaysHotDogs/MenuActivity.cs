17590f4cfc9aebf8b6fe0c7a7ecd2d1089326793
AK33M <akeemtaiwo@outlook.com>
2017-04-04 12:02:50 +0100

    partial final module

--- RaysHotDogs/MenuActivity.cs
+++ RaysHotDogs/MenuActivity.cs
@@ -12,7 +12,7 @@ using Android.Widget;
 
 namespace RaysHotDogs
 {
-    [Activity(Label = "MenuActivity", MainLauncher = true)]
+    [Activity(Label = "Ray's Hot Dogs", MainLauncher = true, Icon ="@drawable/smallIcon")]
     public class MenuActivity : Activity
     {
         private Button orderButton;
